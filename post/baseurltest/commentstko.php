d31fb9c9260654633b36027d5bad8b9d08ba4921
Mike3rd <91030194+Mike3rd@users.noreply.github.com>
2025-01-07 17:33:15 -0500

    fix comments

--- post/baseurltest/commentstko.php
+++
@@ -1,9 +0,0 @@
-			<div class="comments"></div>
-			<script src="https://easyhrcomms.com/post/phpcomments/comments.js"></script>
-			<script>
-			new Comments({
-				page_id: 1,
-				// comments_to_show: 15,
-				// sort_by: newest | oldest | votes,
-			});
-			</script>	
